0a976a452fd59eb22042cc21b4feddc5438afe7c
anbe213 <41134550+anbe213@users.noreply.github.com>
2018-09-29 18:19:57 +0700

    3.0
    29/9/2018

--- src/source/DictionaryManagement.java
+++ src/source/DictionaryManagement.java
@@ -26,7 +26,7 @@ class DictionaryManagement {
         }
     }
     public static void insertFromFile() throws FileNotFoundException {
-        File file = new File("C:\\Users\\Thu\\IdeaProjects\\Dictionary\\dictionaries.txt");
+        File file = new File("C:\\Users\\Administrator.SCX1JKX2YEHIZ1G\\IdeaProjects\\Dictionary\\dictionaries.txt");
         Scanner scan = new Scanner(file);
         while(scan.hasNext()) {
             String eng = scan.next();
@@ -38,7 +38,62 @@ class DictionaryManagement {
             dict.words.add(word);
         }
     }
-    static void dictionaryLookup(){
-
+    public static void dictionaryLookup(){
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Choose a word to look up: ");
+        System.out.println("(press -enough to stop)");
+        do {
+            String wordToLookUp = scan.nextLine();
+            boolean found = false;
+            if (wordToLookUp.equals("-enough")) break;
+            for (int i=0 ; i<dict.size ; i++){
+                if (dict.words.get(i).getWord_target().equals(wordToLookUp)){
+                    System.out.println(dict.words.get(i).getWord_target() + " ===== " + dict.words.get(i).getWord_explain());
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) System.out.println("Can not find your word");
+        } while (true);
+    }
+    public static void deleteWord(){
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Choose a word to delete: ");
+        System.out.println("(press -enough to stop)");
+        do {
+            String wordToDelete = scan.nextLine();
+            boolean found = false;
+            if (wordToDelete.equals("-enough")) break;
+            for (int i=0; i<dict.size; i++){
+                if (dict.words.get(i).getWord_target().equals(wordToDelete)){
+                    dict.words.remove(i);
+                    dict.size--;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) System.out.println("Can not find your word");
+             else System.out.println("Your word has been deleted from the dictionary");
+        } while (true);
+    }
+    public static void Search(){
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Type part of a word to search: ");
+        System.out.println("(press -enough to stop)");
+        do {
+            String wordToSearch = scan.nextLine();
+            if (wordToSearch.equals("-enough")) break;
+            int length = wordToSearch.length();
+            boolean found = false;
+            for (int i=0 ; i<dict.size ;i++){
+                if (length <= dict.words.get(i).getWord_target().length()){
+                    if (dict.words.get(i).getWord_target().substring(0,length).equals(wordToSearch)){
+                        System.out.println(dict.words.get(i).getWord_target());
+                        found = true;
+                    }
+                }
+            }
+            if (!found) System.out.println("Can not find any word");
+        } while (true);
     }
 }
